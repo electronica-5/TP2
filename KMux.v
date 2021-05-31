f62ccbd6f185aa055529a0c95d4165b2c6aa9e4d
Ian Diaz <idiaz@itba.edu.ar>
2021-05-31 17:07:44 -0300

    Agregado lo de manu al diagrama principal
    ya esta probado y ya esta andando (ARI+IAN+MANU)

--- KMux.v
+++ KMux.v
@@ -3,11 +3,10 @@ module KMux (X,Y,KMx, OUT);
 	input [15:0] X, Y;
 	input KMx;
 	output reg [15:0] OUT;
-
-
+	
 	always @ (X or Y or KMx)
 	begin
-	OUT = KMx ? X : Y; //Si Kmx = 0 -> OUT = X
+	OUT = KMx ? Y : X; //Si Kmx = 0 -> OUT = X
 	end
 
 endmodule
